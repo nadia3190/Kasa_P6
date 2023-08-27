81d945416809eaf6487ee75f9a04aa99d59276bc
nadia3190 <nadia.tlb.90@gmail.com>
2023-08-27 10:53:17 +0200

    project finished

--- kasa/src/components/Banner/Banner.js
+++ kasa/src/components/Banner/Banner.js
@@ -1,14 +1,18 @@
-import React from "react";
-import "./Banner.scss"
+import React from 'react';
+import './Banner.scss';
 
 const Background = (props) => {
-    return (
-        <div className="Background">
-            <img src={props.image} className="Background-image" alt="Paysage en plan large" />
-            {props.title && <h1 className="Background-title">{props.title}</h1>}
-        </div>
-    );
-}
+  return (
+    <div className="Background">
+      <img
+        src={props.image}
+        className="Background-image"
+        alt="Paysage en plan large"
+      />
+      {props.title && <h1 className="Background-title">{props.title}</h1>}
+    </div>
+  );
+};
 
-
-export default Background
+export default Background;
+//props est un objet qui contient toutes les propriétés passées à notre composant.
